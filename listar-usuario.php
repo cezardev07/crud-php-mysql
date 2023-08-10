74b5671a23307d79cf8ecc14a530e17ab33a237a
cezardev07 <cezardev07@gmail.com>
2023-08-09 22:11:42 -0300

    feat: creating table of users database mysql

---
+++ listar-usuario.php
@@ -0,0 +1,44 @@
+<h1>LISTAR USUARIOS</h1>
+
+<?php 
+    $sql = "SELECT * FROM usuarios";
+
+    $res = $conn -> query($sql);
+    $qtd = $res -> num_rows;
+
+    if($qtd > 0){
+        print "<table class='table table-hover table-striped table-bordered'>";
+            print "<tr>";
+                print "<th>". "#"                  . "</th>";
+                print "<th>". "Nome"               . "</th>";
+                print "<th>". "Email"              . "</th>";
+                print "<th>". "Data de Nascimento" . "</th>";
+                print "<th>". "Ações"              . "</th>";
+            print "</tr>";
+            while($row = $res -> fetch_object()){
+                print "<tr>";
+                    print "<td>". $row -> id        . "</td>";
+                    print "<td>". $row -> nome      . "</td>";
+                    print "<td>". $row -> email     . "</td>";
+                    print "<td>". $row -> data_nasc . "</td>";
+                    print "<td class = 'd-flex justify-content-center align-items-center'>
+                        <button 
+                            class='btn btn-success' 
+                            onclick=\"location.href='?page=editar&id=" . $row -> id . "'\" 
+                            style='margin-right:10px;'
+                        > Editar </button>
+                        <button 
+                            class='btn btn-danger'
+                            onclick=\" 
+                                if(confirm('Tem certeza que deseja excluir?')){
+                                    location.href='?page=salvar&acao=excluir&id=" . $row -> id . "'
+                                }else{false;}\" 
+                        > Excluir </button>
+                    </td>";
+                print "</tr>";
+            }
+        print "</table>";
+    } else {
+        print "<p class='alert alert-danger'>Não encontrou resultados!</p>";
+    }
+?>
